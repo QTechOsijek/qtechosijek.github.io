5ae2b4d184505493715ba95baeb66b95209b4e73
notASneakyBastard <filip.pilot@gmail.com>
2017-11-18 21:23:23 +0100

    quick style fix no.2

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,7 +26,7 @@ export class Home extends Component {
         <div style={{ marginTop: '35px', paddingBottom: '25px', width: window.innerWidth, backgroundColor: '#4CAF50', color: 'white' }}>
           <h2 style={{ marginLeft: '40px', fontSize: '50px' }}>What are we doing?</h2>
           <ul style={{ fontSize: '35px', marginLeft: '80px' }}>
-            <li style={{ margin: '35px' }}>Mergeing our individual projects and extending them for wider usage</li>
+            <li style={{ margin: '35px' }}>Merging our individual projects and extending them for wider usage</li>
             <li style={{ margin: '35px' }}>Motivating others</li>
             <li style={{ margin: '35px' }}>Learning and having fun</li>
           </ul>
